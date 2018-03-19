f8318584fd990fa2562d5438cd929dc72c67206c
jylei <jlei91@gmail.com>
2018-03-18 21:19:02 -0700

    base form

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Word Translator</h1>
+    <h5>Powered By Vue.js</h5>
+    <TranslateForm></TranslateForm>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm';
+
+export default {
+  name: 'App',
+  components: {
+    TranslateForm
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="translateForm">
+      <form v-on:submit="formSubmit">
+          <!-- binding this to the data at export by using v-model -->
+          <input type = "text" v-model="textToTranslate" placeholder="Enter a word">
+          <input type ="submit" value="Translate">
+      </form>
+  </div>
+</template>
+
+<script>
+// 13:33
+export default {
+  name: 'translateForm',
+  data() {
+      return {
+          textToTranslate: ''
+      }
+  },
+  methods: {
+      formSubmit(e) {
+          alert('Submitted');
+        e.preventDefault();
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
