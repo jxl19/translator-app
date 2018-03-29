95c7731f2ed605a75bd7853baf5fba185f2d578e
jylei <jlei91@gmail.com>
2018-03-28 22:27:01 -0700

    output translation

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,29 @@
     <h5>Powered By Vue.js</h5>
     <!-- onsubmit runs translate text method made below -->
     <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+    <TranslateOutput v-text="translatedText"></TranslateOutput>
   </div>
 </template>
 
 <script>
-import TranslateForm from './components/TranslateForm';
-
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
 export default {
   name: 'App',
   components: {
-    TranslateForm
+    TranslateForm,
+    TranslateOutput
+  },
+  data:function(){
+    return{
+      translatedText: ''
+    }
   },
   methods: {
     translateText: function(text){
       this.$http.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${YANDEX_API_KEY}&lang=ru&text=${text}`)
       .then((res) => {
-        console.log(res.body.text[0]);
+        this.translatedText = res.body.text[0];
       })
     }
   }
